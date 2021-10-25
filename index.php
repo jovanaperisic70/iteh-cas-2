6ec0f0ca286fe1fedd22659b1fc71c583d9ffd19
Jovana Perisic <jovanaperisic18@gmail.com>
2021-10-25 16:34:56 +0200

    greska nadjena

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ if(isset($_POST['username']) && isset($_POST['password'])){
     $uname=$_POST['username'];
     $upass=$_POST['password'];
 
-    $conn=new mysqli();
-
-    $korisnik = new User(null,$uname,$upass);
+    $korisnik = new User(1,$uname,$upass);
     //$odg=$korisnik->logInUser($uname,$upass,$conn);
     $odg=User::logInUser($korisnik,$conn); //ovo je pristup statickim funkcijama preko klase
 
