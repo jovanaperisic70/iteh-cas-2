1c5abfb5027ccb4c7406028a06772a9ef907c7c3
Jovana Perisic <jovanaperisic18@gmail.com>
2021-10-26 17:45:22 +0200

    Izmenjena home stranica sa tabelom

--- home.php
+++ home.php
@@ -1,4 +1,26 @@
+<?php
+require "dbBroker.php";
+require "model/prijava.php";
 
+session_start();
+if(isset($_SESSION['user_id'])){
+    header('Location:index.php');
+    exit();
+}
+
+$rezultat=Prijava::getAll($conn);
+if(!$rezultat){
+    echo "Nastala je greska prilikom izvodjenja upita <br>";
+    die();
+}
+
+if($rezultat->num_rows==0){
+    echo "Nema prijava na kolokvijume";
+    die();
+}
+else {
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,7 +68,9 @@
             </tr>
             </thead>
             <tbody>
-            
+            <?php
+            while($red=$rezultat->fetch_array()):
+            ?>
                 <tr>
                     <td><?php echo $red["predmet"] ?></td>
                     <td><?php echo $red["katedra"] ?></td>
@@ -60,6 +84,10 @@
                     </td>
 
                 </tr>
+                <?php
+                endwhile;
+            }
+                ?>
             </tbody>
         </table>
         <div class="row" >
