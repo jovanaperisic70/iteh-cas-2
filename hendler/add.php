4e00bb574ae35844d499ded2a8ef3e8e1ebec84c
Jovana Perisic <jovanaperisic18@gmail.com>
2021-10-26 17:44:47 +0200

    Kreirani hendleri za SQL funkcije

---
+++ hendler/add.php
@@ -0,0 +1,15 @@
+<?php
+require "../dbBroker.php";
+require "../model/prijava.php";
+
+if(isset($_POST['predmet']) && isset($_POST['katedra']) && isset($_POST['sala']) && isset($_POST['datum'])){
+    $prijava=new Prijava(null,$_POST['predmet'],$_POST['katedra'],$_POST['sala'],$_POST['datum']);
+    $status = Prijava::add($prijava,$conn);
+    if($status){
+        echo 'Succes';
+    }else{
+        echo $status;
+        echo 'Failed';
+    }
+}
+?>
